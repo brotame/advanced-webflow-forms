5bd80b5c4000fa4ba2b6e2998124db24e9ac9a4f
Alex Iglesias <clientes@brota.me>
2020-06-14 20:27:17 +0200

    Update notes.js

--- conditional-logic/src/notes.js
+++ conditional-logic/src/notes.js
@@ -12,3 +12,44 @@ const conditions = [
 ];
 
 const actions = ['show', 'hide', 'enable', 'disable', 'require', 'unrequire'];
+
+const logic = [
+  {
+    conditions: [
+      { selector: '#email', operator: 'equal', value: 'test@test.com' },
+      { selector: '#name', operator: 'not-equal', value: 'Alex' },
+    ],
+    operator: 'and',
+    actions: [
+      {
+        selector: '#phone',
+        action: 'show',
+        clear: false,
+      },
+    ],
+  },
+];
+
+const params = {
+  submitHidden: false,
+};
+
+var conditionalLogic = new ConditionalLogic({
+  logicList: [
+    {
+      conditions: [
+        { origin: '#email', operator: 'equal', value: 'test@test.com' },
+        { origin: '#name', operator: 'not-equal', value: 'Alex' },
+      ],
+      operator: 'and',
+      actions: [
+        {
+          selector: '#phone',
+          action: 'show',
+          clear: false,
+        },
+      ],
+    },
+  ],
+  submitHidden: true,
+});
